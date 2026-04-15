d7220f43d451666bd79c902ef5d1e1ab4061a18d
shwetgaikwad20 <shwetgaikwad.eidiko@gmail.com>
2026-04-15 16:30:13 +0530

    Modified app.py

--- demo-python-app/app.py
+++ demo-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Pratham!'
+    return 'Hello, Pratham! This is the changed app.'
 
 if __name__ == '__main__':
     app.run()
